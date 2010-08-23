3c2abb64d02af36346c15859c727c32555d2ccf2
Tim Caswell <tim@creationix.com>
2010-08-22 23:39:08 -0700

    Calling it a night.
    Got the volumes listing and the book listing parsing.

--- app.js
+++ app.js
@@ -45,10 +45,8 @@ function load(pathname, callback) {
 
 // Simple wrapper around node's http client, htmlparser, jsdom, and sizzle
 function fetch(pathname, callback) {
-  console.log("Fetching: " + pathname);
   load(pathname, function (err, html) {
     if (err) return callback(err);
-    console.log("Loaded %s bytes of html", html.length);
     var browser = index.windowAugmentation(dom, {parser: htmlparser});
     browser.document.body.innerHTML = html;
     var Sizzle = sizzleInit(browser, browser.document);
@@ -57,46 +55,36 @@ function fetch(pathname, callback) {
 }
 
 // Grabs the main sections from the index page
-function getVolumes(callback) {
-  var url = "/";
+function scrape(url, callback) {
   fetch(url, function (err, Sizzle) {
     if (err) return callback(err);
-    var results = Hash.new();
-    Sizzle("a").forEach(function (a) {
-      var href = Url.resolve(url, a.href);
-      if (!(/\/contents$/).test(href)) return;
-      Sizzle(".smallcaps", a).forEach(function (div) {
-        results[href] = div.innerHTML.trim();
-      })
+    var base = Url.parse(Sizzle("base")[0].href || url).pathname;
+    var result = Hash.new();
+    var scope = Sizzle(".contents")[0];
+    Sizzle("a", scope).forEach(function (a) {
+      var href = a.href;
+      var name = a.textContent.trim();
+      var match = href.match(/(.*)\/contents/);
+      var code;
+      if (match) {
+        code = match[1];
+      } else {
+        code = href;
+      }
+      result[code] = {name: name, href: Url.resolve(base, href)}
     });
-    callback(null, results);
+    callback(null, result);
   });
 }
 
-function getBooks(url, callback) {
-  fetch(url, function (err, Sizzle, document) {
-    if (err) return callback(err);
-    var base = Url.parse(Sizzle("base")[0].href || url).pathname;
-    Sizzle("a").forEach(function (a) {
-      var href = Url.resolve(base, a.href);
-      if (href[0] !== "/") return;
-      console.log(href)
-      console.log(a.innerHTML.trim());
-      // if (!(/^))
-    });
-    // console.log(document.innerHTML);
-  });
-}
 
 function onLoad() {
-  getVolumes(function (err, volumes) {
+  scrape("/", function (err, volumes) {
     if (err) throw err;
-    volumes.forEach(function (name, url) {
-      getBooks(url, function (err, books) {
-        if (err) throw err;
-        console.log("\n" + name);
-        console.dir(books);
-      });
+    console.dir(volumes);
+    scrape(volumes.bm.href, function (err, books) {
+      if (err) throw err;
+      console.dir(books);
     });
   });
 }
